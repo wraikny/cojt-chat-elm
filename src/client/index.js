6eec8aab389ab9effd0f717cf2dbf545dfe347be
wraikny <writtenknown@gmail.com>
2019-05-08 13:27:01 +0900

    login処理 in client js

--- src/client/index.js
+++ src/client/index.js
@@ -6,32 +6,51 @@ var socket = io();
 
 let debugMode = true;
 
+let debugPrint = x => {
+  if(debugMode) {
+    console.log(x);
+  }
+};
+
+
+
 app.ports.sendMessage.subscribe( msg => {
   const json = JSON.stringify(msg);
   socket.emit('chat message', json);
 
-  console.log("sendMessage: " + msg);
+  debugPrint("sendMessage: " + msg);
 });
 
 socket.on('chat message', msg => {
   app.ports.receiveMessage.send(msg);
   window.scrollTo(0, document.body.scrollHeight);
 
-  console.log("receiveMessage: " + msg);
+  debugPrint("receiveMessage: " + msg);
 });
 
-app.ports.sendLogin.subscribe( name => {
-  const json = JSON.stringify(name);
-  socket.emit('new login', json);
 
-  console.log("sendLogin: " + name);
+// name : string
+app.ports.sendLogin.subscribe( name => {
+  socket.emit('attempt login', name);
+
+  debugPrint("attemptLogin: " + name);
+});
+
+socket.on('success login', id => {
+  app.ports.receiveLoginSuccess.send(id);
+  debugPrint("successLogin: " + id);
+});
+
+socket.on('failed login', msg => {
+  app.ports.receiveLoginFailed.send(msg);
+  debugPrint("successLogin: " + msg);
 });
 
 socket.on('new login', name => {
   app.ports.receiveLogin.send(name);
   window.scrollTo(0, document.body.scrollHeight);
 
-  console.log("receiveLogin: " + name);
+  debugPrint("receiveLogin: " + name);
 });
 
 
@@ -39,5 +58,5 @@ socket.on('server log', json => {
   app.ports.loginResult.send(json);
   window.scrollTo(0, document.body.scrollHeight);
 
-  console.log("loginResult: " + najsonme);
+  debugPrint("loginResult: " + najsonme);
 });
